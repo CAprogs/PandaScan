b0091484ec8b1181c80f8a91b37cc7487d032d0a
Charles <charlesalbertkottoekambi@gmail.com>
2023-08-25 23:25:12 +0200

    Delete PandaScan/datas directory

--- PandaScan/datas/lelscans.net/Update_lelscans.py
+++
@@ -1,8 +0,0 @@
-from Scrap_Chapters_lelscans import Scrap_Chapters
-from Scrap_Titles_lelscans import Scrap_Titles
-from Changelog_lelscans import generate_changelog
-
-def Update_lelscans():
-    Scrap_Titles()
-    Scrap_Chapters()
-    generate_changelog()
--- PandaScan/datas/scantrad-vf/Update_scantrad.py
+++
@@ -1,8 +0,0 @@
-from Scrap_Chapters_scantrad import Scrap_Chapters
-from Scrap_Titles_scantrad import Scrap_Titles
-from Changelog_scantrad import generate_changelog
-
-def Update_scantrad():
-    Scrap_Titles()
-    Scrap_Chapters()
-    generate_changelog()
