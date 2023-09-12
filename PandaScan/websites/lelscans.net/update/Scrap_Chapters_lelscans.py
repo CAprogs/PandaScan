fbb65fdeb661ffdcd37f3d5dbe665dced54546d3
Charles <charlesalbertkottoekambi@gmail.com>
2023-09-12 23:55:00 +0200

    Update Scrap_Chapters_lelscans.py

--- PandaScan/websites/lelscans.net/update/Scrap_Chapters_lelscans.py
+++ PandaScan/websites/lelscans.net/update/Scrap_Chapters_lelscans.py
@@ -21,7 +21,7 @@ def Scrap_Chapters():
             soup = BeautifulSoup(html_content, "html.parser")
             select_element = soup.select_one('#header-image > h2 > form > select:nth-child(1)')
             
-            print(f"\nManga : {manga_name}") # Indique dans quel manga nous sommes pour le scrapping des chapitres
+            print(f"\nManga : {manga_name}\n") # Indique dans quel manga nous sommes pour le scrapping des chapitres
             manga_chapters_dict[manga_name]=[] # Crée une clé de dictionnaire vide , avec le nom du manga qu'on explore.
 
             if select_element and select_element.name == "select":
@@ -30,23 +30,25 @@ def Scrap_Chapters():
                     chapter = "chapitre " + desired_part
                     manga_chapters_dict[manga_name].append(chapter) # Ajouter le chapitre à 'manga_chapters_dict' avec sa clé correspondante
                     print(f"{chapter} récupéré ")
-                
-                # Convertir le dictionnaire en document YAML
-                yml_data = yaml.dump(manga_chapters_dict)
-
-                print(f"\nSauvegarde des datas ...")
-
-                # Sauvegarde des datas
-                with open(f'{script_repo}/datas/mangas_chapters_temp.yml', 'w') as file:
-                    file.write(yml_data)
-
-                print(f"\nFin Scrapping.")
             
             else:
                 print(f"Erreur, aucun chapitre trouvé | {manga_name}")
-                     
+        
         except:
             print(f"An Error occurred ! Please Debug | {script_repo}")
+            break
+    
+    # Convertir le dictionnaire en document YAML
+    yml_data = yaml.dump(manga_chapters_dict)
+
+    print(f"\nSauvegarde des datas ...")
+
+    # Sauvegarde des datas
+    with open(f'{script_repo}/datas/mangas_chapters_temp.yml', 'w') as file:
+        file.write(yml_data)
+
+    print(f"\nFin Scrapping.")
+    
 
 
 # Uncomment to debug
